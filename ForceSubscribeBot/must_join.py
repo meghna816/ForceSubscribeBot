0fe4248e773f57e8a1dddb9138603e16793f85a7
meghna816 <tannameghna1@gmail.com>
2025-01-08 12:27:20 +0530

    Update must_join.py

--- ForceSubscribeBot/must_join.py
+++ ForceSubscribeBot/must_join.py
@@ -1,32 +1,45 @@
+#must_join.py
+import logging
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from Config import MUST_JOIN
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @Client.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not MUST_JOIN:  # Not compulsory
+    if not MUST_JOIN:  # If MUST_JOIN is not set, do nothing
         return
+
+    # Try to check if the user is part of the required channel
     try:
-        try:
-            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
-        except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
-            else:
+        await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+    except UserNotParticipant:
+        # If user is not a participant, send the invite link
+        if MUST_JOIN.isalpha():
+            link = f"https://t.me/{MUST_JOIN}"
+        else:
+            try:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
-            try:
-                await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
-                    ])
-                )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
+            except Exception as e:
+                logger.error(f"Error fetching chat info for {MUST_JOIN}: {e}")
+                return  # If something goes wrong, just return
+
+        try:
+            await msg.reply(
+                f"You must join [this channel]({link}) to use me. After joining, try again!",
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
+                ])
+            )
+            await msg.stop_propagation()  # Prevent further message processing
+        except ChatWriteForbidden:
+            logger.warning(f"Cannot send message to {msg.chat.id}, write access forbidden.")
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
+        logger.error(f"I'm not admin in the MUST_JOIN chat: {MUST_JOIN}")
+
